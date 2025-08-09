3b2c37afa199d52b2c072fe71b2fa9c05e839d8d
mowler <luce@rose>
2025-08-09 19:07:49 +0100

    init

---
+++ app/src/main/java/com/example/guidingtool/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.guidingtool
+
+object Routes {
+    // Intro screen, learn more about the user
+    const val Name = "name" // Get user's name
+    const val Region = "region" // Get users locale/region
+    const val Term = "term" // Get term dates from the user
+    const val Done = "done" // What displays after new user enters data
+}
