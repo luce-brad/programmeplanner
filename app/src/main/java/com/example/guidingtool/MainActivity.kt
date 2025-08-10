63506c8b1ee3252552d668d86926e8807ba90a32
lb <lb@rose>
2025-08-10 20:20:39 +0100

    refactor: add UI routes as import

--- app/src/main/java/com/example/guidingtool/MainActivity.kt
+++ app/src/main/java/com/example/guidingtool/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.guidingtool.nav.Routes
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
